cf836d943493e5f52ef3837ea79e3a722f6cc12c
Sue <sueyoung.6311@gmail.com>
2019-03-27 20:21:06 -0400

    Initial commit

---
+++ src/main/java/com/bysue/springboot_301/Springboot301Application.java
@@ -0,0 +1,13 @@
+package com.bysue.springboot_301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot301Application.class, args);
+	}
+
+}
